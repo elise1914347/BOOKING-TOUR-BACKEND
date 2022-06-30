73beb6ee5c7036492f118ce36e493d6a37c175fc
elise1914347 <eliseduse@gmail.com>
2022-06-30 10:25:20 +0200

    backend

--- src/services/userService.js
+++ src/services/userService.js
@@ -4,6 +4,7 @@ class UserServiceS{
 
 static testServiceFunction(req){
     req.body.names=req.body.names.toUpperCase();
+    req.body.Home=req.body.Home.toUpperCase();
 const {num1,num2}=req.body;
 const sum=num1 + num2 ;
 req.body.sum= sum;
